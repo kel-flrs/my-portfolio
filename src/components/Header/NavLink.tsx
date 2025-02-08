0cd7e6410891c2b3bbd9271bcfb391f873b7433b
dev-jmichael <flores.johnmichael.c.0511@gmail.com>
2025-02-08 18:36:36 +0800

    add link hover color

--- src/components/Header/NavLink.tsx
+++ src/components/Header/NavLink.tsx
@@ -11,8 +11,8 @@ export default function NavLink({ href = "", label = "", className = "", mobile 
     <a
       href={href}
       className={`${className} ${
-        mobile ? "block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium hover:bg-white hover:border-white" : "hover:text-white"
-      } text-white hover:text-darkPurple transition-all delay-75 ease-in-out`}
+        mobile ? "block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium hover:bg-white hover:border-white" : "hover:text-darkPurple transition-all delay-75 ease-in-out"
+      }`}
     >
       {label}
     </a>
